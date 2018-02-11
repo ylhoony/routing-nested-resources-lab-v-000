a463ce9a18ac9e344547b8e3ff6dc15458ba33a9
Hoon Lee <yl.hoony@gmail.com>
2018-02-11 00:52:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
       @song = @artist.songs.find_by(id: params[:id])
 
       if @song.nil?
-        redirect_to artist_songs_path(artist), alert: "Song not found"
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
     else
       @song = Song.find(params[:id])
