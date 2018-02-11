b726b04ab7b66d4dd1b4428a9c3fa3222f4780c2
Hoon Lee <yl.hoony@gmail.com>
2018-02-11 01:09:03 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,17 +19,25 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      # begin
-      #   artist = Artist.find(params[:artist_id])
-      # rescue ActiveRecord::RecordNotFound => e
-      #   artist = nil
-      # end
-      @artist = Artist.find(params[:artist_id])
-      @song = @artist.songs.find_by(id: params[:id])
+      begin
+        artist = Artist.find(params[:artist_id])
+      rescue ActiveRecord::RecordNotFound => e
+        artist = nil
+      end
 
-      if @song.nil?
+      if artist
+        @songs = artist.songs
+      else
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
+
+      
+      # @artist = Artist.find(params[:artist_id])
+      # @song = @artist.songs.find_by(id: params[:id])
+      # 
+      # if @song.nil?
+      #   redirect_to artist_songs_path(@artist), alert: "Song not found"
+      # end
     else
       @song = Song.find(params[:id])
     end
