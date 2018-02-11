81f19a6fb0802b1f938f405dc5b5d9c5e449c1c7
Hoon Lee <yl.hoony@gmail.com>
2018-02-11 00:31:55 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,39 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      begin
+        artist = Artist.find(params[:artist_id])
+      rescue ActiveRecord::RecordNotFound => e
+        artist = nil
+      end
+
+      if artist
+        @songs = artist.songs
+      else
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      begin
+        artist = Artist.find(params[:artist_id])
+      rescue ActiveRecord::RecordNotFound => e
+        artist = nil
+      end
+
+      if artist
+        @songs = artist.songs
+      else
+        flash[:alert] = "Songs are not found."
+        redirect_to artist_songs_path
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
@@ -50,4 +79,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
