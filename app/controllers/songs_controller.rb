d3de2bc0083aa19d4dc11dce489fcf253e9f238b
Hoon Lee <yl.hoony@gmail.com>
2018-02-11 01:09:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -31,10 +31,10 @@ class SongsController < ApplicationController
         redirect_to artist_songs_path(@artist), alert: "Song not found"
       end
 
-      
+
       # @artist = Artist.find(params[:artist_id])
       # @song = @artist.songs.find_by(id: params[:id])
-      # 
+      #
       # if @song.nil?
       #   redirect_to artist_songs_path(@artist), alert: "Song not found"
       # end
